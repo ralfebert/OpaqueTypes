b8d6c0ea560f3901bd06802c6928f2c747740e0d
Ralf Ebert <info@ralfebert.de>
2019-09-02 17:20:01 +0200

    Project setup OpaqueTypes

---
+++ OpaqueTypes/App/OpaqueTypesAppDelegate.swift
@@ -0,0 +1,14 @@
+// © 2019 Ralf Ebert — OpaqueTypes
+
+import UIKit
+
+@UIApplicationMain
+class OpaqueTypesAppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+
+}
